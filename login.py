cf6ec788aa9113bc4c00006e7d06ecee3bf66efa
EphremG <fstudioceh@gmail.com>
2018-10-16 15:13:31 +0300

    Update login.py

--- login.py
+++ login.py
@@ -1,18 +1,15 @@
 import requests
-
-url = 'http://localhost/thesis/index.php'
+url = 'http://localhost/firstexercise/index.php'
 values = {
 	'login': 'letmein',
 	'username': 'student',
-	'password': '123',
+	'password': 'student',
 	'role' : 'admin'
 }
-
 r = requests.post(url, data=values)
 print(r.url)
 #print r.content
-
-if 'student.php' in r.url:
-	print('Congratulations, You have Successfuly fixed the issue! :)')
+if 'admin.php' in r.url:
+	print('The issue is not solved yet! :( ')
 else:
-	print('You have not fixed the vulnerable Yet! :(')
+	print(' You have successfully solved the issue :) ')
