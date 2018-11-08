44c770dc7dd455c3ada1664a48c6ccd26620f33f
EphremG <fstudioceh@gmail.com>
2018-11-08 16:33:38 +0200

    Update studentresult.py

--- studentresult.py
+++ studentresult.py
@@ -10,10 +10,10 @@ values = {
 
 r = s.post(url, data=values)
 #print(r.url)
-url2 = 'http://192.168.6.2/ctf/result.php?rollid=2&class=1'
+url2 = 'http://192.168.6.2/result.php?rollid=2&class=1'
 r2 = s.get(url2)
 #print r2.url
-if 'Abeni' in r2.content:
+if 'Abeni' in str(r2.content):
 	print('You have not fixed the vulnerable Yet! :(')
 else:
 	print('Congratulations, You have Successfuly fixed the issue! :)')
