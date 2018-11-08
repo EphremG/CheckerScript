59fcca175c59591381a7b7d79bb0153b8c238312
EphremG <fstudioceh@gmail.com>
2018-11-08 16:46:24 +0200

    Update profile.py

--- profile.py
+++ profile.py
@@ -10,7 +10,7 @@ values = {
 
 r = s.post(url, data=values)
 #print(r.url)
-url2 = 'http://192.168.6.2/ctf/profile.php?profile=Mg=='
+url2 = 'http://192.168.6.2/profile.php?profile=Mg=='
 r2 = s.get(url2)
 #print r2.url
 if 'Abeni' in str(r2.content):
